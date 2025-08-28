03f2ccd5bf8cd9909bb6ba85b5449343e3c02984
JoaoMateusREG <joaomateus1948@hotmail.com.com>
2025-08-28 17:39:26 -0300

    tentativa de conexao com backend 4

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   // Configuração de CORS
   app.enableCors({
-    origin: ['http://localhost:3418'],
+    origin: ['http://138.2.244.250:3418'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     credentials: true,
